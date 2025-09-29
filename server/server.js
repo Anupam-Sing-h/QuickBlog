b204530f6bf8ed4ecf049d199cefa3173ef324b4
Anupam Singh <anupamsingh584210@gmail.com>
2025-09-29 12:47:03 +0530

    getting the old format of origin

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ await connectDb();
 //Middlewars
 //app.use(cors())
 app.use(cors({
-  origin: ['http://localhost:5173' , 'https://quick-blog-seven-beta.vercel.app', 'quick-blog-git-main-anupam-singhs-projects-94cb78b6.vercel.app', 'quick-blog-b37vshl3n-anupam-singhs-projects-94cb78b6.vercel.app', 'quick-blog-server-mu-three.vercel.app', 'quick-blog-server-git-main-anupam-singhs-projects-94cb78b6.vercel.app', 'quick-blog-server-kkkze3g0l-anupam-singhs-projects-94cb78b6.vercel.app'],
-  //origin: 'https://quick-blog-seven-beta.vercel.app',
+  //origin: ['http://localhost:5173' , 'https://quick-blog-seven-beta.vercel.app', 'quick-blog-git-main-anupam-singhs-projects-94cb78b6.vercel.app', 'quick-blog-b37vshl3n-anupam-singhs-projects-94cb78b6.vercel.app', 'quick-blog-server-mu-three.vercel.app', 'quick-blog-server-git-main-anupam-singhs-projects-94cb78b6.vercel.app', 'quick-blog-server-kkkze3g0l-anupam-singhs-projects-94cb78b6.vercel.app'],
+  origin: ['http://localhost:5173','https://quick-blog-seven-beta.vercel.app'],
   credentials: true                // ✅ allow cookies/auth headers
 }));
 
